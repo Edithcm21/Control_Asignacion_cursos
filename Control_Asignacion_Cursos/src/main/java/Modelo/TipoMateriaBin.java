109dd0d2a920b726c36370a77709c8ef22445e55
Edith <edithcm_21@hotmail.com>
2023-05-15 17:24:38 -0600

    JavaBin

---
+++ Control_Asignacion_Cursos/src/main/java/Modelo/TipoMateriaBin.java
@@ -0,0 +1,39 @@
+package Modelo;
+
+import java.io.Serializable;
+
+public class TipoMateriaBin implements Serializable {
+    private int ID_Tipo;
+    private String Descripcion;
+
+    //Insertar
+    public TipoMateriaBin(String descripcion) {
+        Descripcion = descripcion;
+    }
+    //Select and delete
+
+    public TipoMateriaBin(int ID_Tipo) {
+        this.ID_Tipo = ID_Tipo;
+    }
+    //Modificar y consultar todos los datos
+    public TipoMateriaBin(int ID_Tipo, String descripcion) {
+        this.ID_Tipo = ID_Tipo;
+        Descripcion = descripcion;
+    }
+
+    public int getID_Tipo() {
+        return ID_Tipo;
+    }
+
+    public void setID_Tipo(int ID_Tipo) {
+        this.ID_Tipo = ID_Tipo;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        Descripcion = descripcion;
+    }
+}
